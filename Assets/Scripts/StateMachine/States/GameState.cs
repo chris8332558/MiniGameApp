3598c547040177f8a4108046a23e8fbbea9b611d
chris8332558 <chris8332558@gmail.com>
2025-02-05 16:06:00 -0800

    Press Space to start the game now

--- Assets/Scripts/StateMachine/States/GameState.cs
+++ Assets/Scripts/StateMachine/States/GameState.cs
@@ -1,9 +1,14 @@
 using UnityEngine;
+using System;
+using System.Collections;
+
 
 namespace Chris
 {
     public class GameState : State
     {
+        bool isGameStarted;
+
         public override void Enter()
         {
             Debug.Log("Enter Game State");
@@ -12,7 +17,12 @@ namespace Chris
 
         public override void Update()
         {
-            GameEvents.GameplayTimeTicked?.Invoke(Time.deltaTime);
+            if (Input.GetKeyDown(KeyCode.Space)) isGameStarted = true; // Will change to wait 3 seconds
+            if (isGameStarted)
+            {
+                GameEvents.GameplayTimeTicked?.Invoke(Time.deltaTime);
+            }
+
         }
 
         public override void Exit()
